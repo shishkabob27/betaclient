999b3fe936fe20c47c384ea469fd2e8da4dd39e4
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2025-01-27 17:32:34 -0600

    Create GlowstoneBlock.cs

---
+++ src/World/Blocks/GlowstoneBlock.cs
@@ -0,0 +1,22 @@
+public class GlowstoneBlock : BlockDefinition
+{
+    public override byte ID { get { return 0x59; } }
+    
+    public override double BlastResistance { get { return 1.5; } }
+
+    public override double Hardness { get { return 0.3; } }
+
+    public override byte Luminance { get { return 15; } }
+
+    public override bool Opaque { get { return false; } }
+    
+    public override string GetDisplayName(short metadata)
+    {
+        return "Glowstone";
+    }
+
+    public override Tuple<int, int> GetTextureMap(byte metadata, BlockFace face)
+    {
+        return new Tuple<int, int>(9, 6);
+    }
+}
