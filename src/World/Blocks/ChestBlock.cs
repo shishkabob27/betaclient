57d5a3f882f11bed7d549f5e889f70e4a9c36041
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2025-02-18 21:53:47 -0600

    Create ChestBlock.cs

---
+++ src/World/Blocks/ChestBlock.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+
+public class ChestBlock : BlockDefinition
+{    
+    public override byte ID { get { return 0x36; } }
+    
+    public override double BlastResistance { get { return 12.5; } }
+
+    public override double Hardness { get { return 2.5; } }
+
+    public override byte Luminance { get { return 0; } }
+
+    public override bool Opaque { get { return false; } }
+    
+    public override string GetDisplayName(short metadata)
+    {
+        return "Chest";
+    }
+
+    public override Tuple<int, int> GetTextureMap(byte metadata, BlockFace face)
+    {
+        Logger.Debug("ChestBlock Metadata: " + metadata);
+        return new Tuple<int, int>(10, 1);
+    }
+}
